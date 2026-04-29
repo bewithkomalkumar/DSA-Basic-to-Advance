359d26347d06d01123b903b4c286d2b2fbebacce
komal kumar <er.kumarkomal@gmail.com>
2026-04-29 23:04:01 +0530

    problem1a

--- 01 Calculate Electricity Bill/index.js
+++ 01 Calculate Electricity Bill/index.js
@@ -1,1 +1,22 @@
-console.log("Running...");
+let amount=0;
+let unit=Number( prompt("Enter Electricity Unit"));
+
+if(unit>400){
+	amount+=13*(unit-400);
+	unit=400;
+
+}
+
+if(unit>=201 && unit<=400){
+	amount+=8*(unit-200);
+	unit=200;
+}
+
+if(unit>=101 && unit<=200){
+	amount+=6*(unit-100);
+	unit=100;
+}
+
+amount+=4.2*unit;
+document.getElementById("result").innerText=`Total Bill : ${amount}`;
+console.log(amount)
